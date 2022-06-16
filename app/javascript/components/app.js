82b4c0fecfe3a8af39a9fa94cb2f1090463a7c97
Rick <rickywu209@gmail.com>
2022-06-16 07:29:42 +0000

    change original react files to the new components

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,9 +1,10 @@
 import React, { component } from 'react'
+import PortfolioContainer from './PortfolioContainer'
 
 class App extends Component {
   render(){
     return(
-      <div>Hello World</div>
+      <PortfolioContainer/>
     )
   }
 }
