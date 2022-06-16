a0e504d7ef50efcdd9892f829413c08393776859
Rick <rickywu209@gmail.com>
2022-06-16 05:25:47 +0000

    crypto api add along with the route files

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,11 @@
 class Currency < ApplicationRecord
+  def calculate_value(amount)
+    current_price.to.f * amount.to_f
+  end
+
+  def current_price
+url = 'http://api.coinbase.com/v2/prices/btc-usd/spot'
+request = HTTParty.get(url + self.slug)
+response = JSON.parse(request.body)[0]["price_usd"]
+  end
 end
