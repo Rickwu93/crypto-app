854fea8a075065eb2f1e8e466425c2d310f0b58e
Rick <rickywu209@gmail.com>
2022-06-16 06:16:02 +0000

    component folder created along with helper functions

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,9 +2,29 @@ class CurrenciesController < ApplicationController
   def index
   end
 
+  # post request when users make search requests to compare lower case name vs lower case of search result
+  # send back json of all the currencies to that users' search
   def search
+    @currencies = Currency.where('LOWER(name) Like ?', "%#{params[:search].downcase}%")
+    render json: { currencies: @currencies }
   end
 
+  # Takes in currency id and the amount owned
+  # Returns final calculations
+  #pass back json for our calculate method
   def calculate
+    amount = params[:amount]
+    render json: {
+      currency: currency,
+      current_price: currency.current_price,
+      amount: amount,
+      value: currency.calculate_value(amount)
+    }
+  end
+
+  private
+
+  def currency
+    @currency ||= Currency.find(params[:id])
   end
 end
--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,9 +1,11 @@
 class Currency < ApplicationRecord
   def calculate_value(amount)
-    current_price.to.f * amount.to_f
+    # current crypto price * quantity
+    (current_price.to.f * amount.to_f).round(4)
   end
 
   def current_price
+# api no longer working
 url = 'http://api.coinbase.com/v2/prices/btc-usd/spot'
 request = HTTParty.get(url + self.slug)
 response = JSON.parse(request.body)[0]["price_usd"]
